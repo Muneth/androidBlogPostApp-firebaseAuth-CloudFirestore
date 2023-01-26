23ae384400c78e83a43c3295401ef5873bb1c099
muneth <muneetchaudhary@me.com>
2023-01-26 15:06:07 +0100

    guide removed

--- javadocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.soutnence_3"},{"l":"com.example.soutnence_3.model"},{"l":"com.example.soutnence_3.ui"},{"l":"util"}]
